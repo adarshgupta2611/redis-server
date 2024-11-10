6a59c0fbeab7792acc1d5fca36bc374a93a0977f
adarshgupta2611 <adarshgupta261199@gmail.com>
2024-11-10 10:10:38 +0530

    PONG

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ import socket  # noqa: F401
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()
+    with socket.create_server(("localhost", 6379), reuse_port=True) as server_socket:
+        client_socket, addr = server_socket.accept()
+        client_socket.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
