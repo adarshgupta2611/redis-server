5b2551305e6f2c3cb0676e57cf234b03944bc927
adarshgupta2611 <adarshgupta261199@gmail.com>
2024-11-10 11:12:23 +0530

    Multiple PINGs

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ import socket  # noqa: F401
 def main():
     with socket.create_server(("localhost", 6379), reuse_port=True) as server_socket:
         client_socket, addr = server_socket.accept()
-        client_socket.send(b"+PONG\r\n")
+        while client_socket.recv(1024) is not None:
+            client_socket.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
